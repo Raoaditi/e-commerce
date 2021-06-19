cf7973740d3846f104ea7134c001e1981f1d1526
Raoaditi <62167638+Jalpara@users.noreply.github.com>
2021-06-19 21:04:11 +0530

    mongoose schema created

---
+++ server/models/Product.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    description: {
+        type: String, 
+        required: true
+    },
+    price: {
+        type: Number, 
+        required: true
+    },
+    countInStock: {
+        type: Number, 
+        required: true
+    },
+    imageUrl: {
+        type: String, 
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Products', productSchema);
