0da8a84cf0178339d195fabaece818d8c56744be
Raoaditi <62167638+Jalpara@users.noreply.github.com>
2021-07-08 22:21:11 +0530

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
+import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+//screens
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from "./screens/CartScreen";
+
+//components
+import Navbar from './components/Navbar';
+import Backdrop from './components/Backdrop';
+import SideDrawer from './components/SideDrawer';
+
 
 function App() {
+  const [sideToggle, setSideToggle] = useState(false);
   return (
-    <div className="app">
-      Test commit
-    </div>
+    <Router>
+      
+      <Navbar click={() => setSideToggle(true)} />
+      <SideDrawer show={sideToggle} click={() => setSideToggle(false)} />
+      <Backdrop show={sideToggle} click={() => setSideToggle(false)} />
+      
+      <main className="app">
+        <Switch>
+          <Route exact path = "/" component={ HomeScreen }/>
+          <Route exact path = "/product/:id" component={ ProductScreen }/>
+          <Route exact path = "/cart" component={ CartScreen }/>
+        </Switch>
+      </main>
+
+    </Router>
   );
 }
 
---
+++ client/src/screens/CartScreen.js
@@ -0,0 +1,11 @@
+
+
+const CartScreen = () => {
+    return (
+        <div>
+            CartScreen
+        </div>
+    )
+}
+
+export default CartScreen
